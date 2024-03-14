731d0591ec6a7b58f52708aa6ea9335d9cb9df1a
kristof <krostof4@wp.pl>
2024-03-14 15:52:06 +0100

    tests where added

---
+++ src/test/java/com/example/joboffers/domain/crud/OfferFacadeTest.java
@@ -0,0 +1,12 @@
+package com.example.joboffers.domain.crud;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OfferFacadeTest {
+
+    @Test
+    void f(){}
+
+}
